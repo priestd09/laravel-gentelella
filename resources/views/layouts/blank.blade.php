1d612c74e9e0b34094b1189fb9f457ded077f2ee
Florient Ratel <f.ratel@webetsolutions.com>
2016-06-14 13:56:26 +0200

    Complete blank layout

---
+++ resources/views/layouts/blank.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+        <!-- Meta, title, CSS, favicons, etc. -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Gentellela Alela! | </title>
+
+        @stack('stylesheets')
+
+        @push('stylesheets')
+        <!-- Bootstrap -->
+        <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Font Awesome -->
+        <link href="../vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+        @endpush
+
+        <!-- At the end : Custom Theme Style -->
+        <link href="../build/css/custom.min.css" rel="stylesheet">
+    </head>
+
+    <body class="nav-md">
+        <div class="container body">
+            <div class="main_container">
+
+                @yield('main_container')
+
+            </div>
+        </div>
+
+        @stack('scripts')
+
+        @push('scripts')
+            <!-- jQuery -->
+            <script src="../vendors/jquery/dist/jquery.min.js"></script>
+            <!-- Bootstrap -->
+            <script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+        @endpush
+
+        <!-- At the end : Custom Theme Scripts -->
+        <script src="../build/js/custom.min.js"></script>
+    </body>
+</html>
